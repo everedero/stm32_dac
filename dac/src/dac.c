c69508bd878ff7c8c6ec43c1d8c27d5c92395e48
Eve Redero <eve.redero-ext@tagheuer.com>
2024-07-11 16:49:51 +0200

    wip: sawtooth working, wip dma

--- dac/src/dac.c
+++ dac/src/dac.c
@@ -7,15 +7,29 @@
 #include "stm32f7xx_hal_cortex.h" // for HAL_NVIC EnableIRQ and HAL_NVIC SetPriority
 
 DAC_HandleTypeDef hdac1;
+DMA_HandleTypeDef hdma_dac1;
 
 void Error_Handler(void);
 static void MX_DAC1_Init(void);
 static void MX_DMA_Init(void);
 
+#define NS  128
+
 int init_dac(void)
 {
 
 	uint32_t DAC_OUT[4] = {0, 1241, 2482, 3723};
+	uint32_t Wave_LUT[NS] = {
+		2048, 2149, 2250, 2350, 2450, 2549, 2646, 2742, 2837, 2929, 3020, 3108, 3193, 3275, 3355,
+		3431, 3504, 3574, 3639, 3701, 3759, 3812, 3861, 3906, 3946, 3982, 4013, 4039, 4060, 4076,
+		4087, 4094, 4095, 4091, 4082, 4069, 4050, 4026, 3998, 3965, 3927, 3884, 3837, 3786, 3730,
+		3671, 3607, 3539, 3468, 3394, 3316, 3235, 3151, 3064, 2975, 2883, 2790, 2695, 2598, 2500,
+		2400, 2300, 2199, 2098, 1997, 1896, 1795, 1695, 1595, 1497, 1400, 1305, 1212, 1120, 1031,
+		944, 860, 779, 701, 627, 556, 488, 424, 365, 309, 258, 211, 168, 130, 97,
+		69, 45, 26, 13, 4, 0, 1, 8, 19, 35, 56, 82, 113, 149, 189,
+		234, 283, 336, 394, 456, 521, 591, 664, 740, 820, 902, 987, 1075, 1166, 1258,
+		1353, 1449, 1546, 1645, 1745, 1845, 1946, 2047
+	};
 	uint8_t i = 0;
 
 //	HAL_Init();
@@ -27,6 +41,8 @@ int init_dac(void)
 	/* Clocks, pinctrl, channel count and resolution
 	 * handled by Zephyr driver in drivers/dac/dac_stm32.c
 	 * */
+
+//	HAL_DAC_Start_DMA(&hdac1, DAC_CHANNEL_1, (uint32_t*)Wave_LUT, 128, DAC_ALIGN_12B_R);
 
 	while (1)
 	{
@@ -83,7 +99,26 @@ static void MX_DAC1_Init(void)
 	      Error_Handler();
 	}
 	/* Enable DMA */
-	LL_DAC_EnableDMAReq(DAC1, 1);
+	/* DAC DMA Init */
+	/* DAC1 Init */
+	hdma_dac1.Instance = DMA1_Stream5;
+	hdma_dac1.Init.Channel = DMA_CHANNEL_7;
+	hdma_dac1.Init.Direction = DMA_MEMORY_TO_PERIPH;
+	hdma_dac1.Init.PeriphInc = DMA_PINC_DISABLE;
+	hdma_dac1.Init.MemInc = DMA_MINC_ENABLE;
+	hdma_dac1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+	hdma_dac1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+	hdma_dac1.Init.Mode = DMA_NORMAL;
+	hdma_dac1.Init.Priority = DMA_PRIORITY_LOW;
+	hdma_dac1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	if (HAL_DMA_Init(&hdma_dac1) != HAL_OK)
+	{
+	  Error_Handler();
+	}
+	// LL_DAC_EnableDMAReq(DAC1, 1);
+	hdac1.DMA_Handle1 = &hdma_dac1;
+	hdma_dac1.Parent = &hdac1;
+    	//__HAL_LINKDMA(hdac1, DMA_Handle1, hdma_dac1);
 
 }
 
